61d87fca69a5132569f3b02f229f03ea2b393d13
bamboo <nmz89825@gmail.com>
2023-06-22 12:34:29 +0800

    :lipstick: Basic layout for index page using grid.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,32 @@
 <script setup lang="ts">
+const emojis = ['🤩', '🥳', '😐', '🙁', '🙄', '🤬'];
+const emojiWrapper = ref<HTMLDivElement>();
+const myRate = ref<number>(0);
+function onChange() {
+
+}
 </script>
 
 <template lang="pug">
-h1 hello world
+#app
+  .emoji-wrapper.overflow-hidden.row-span-2.p-2(
+    ref="emojiWrapper"
+  )
+    .emoji.w-18.h-18.flex.justify-center.items-center.snap-y.mb-2.text-7xl(
+      v-for="emoji in emojis"
+      :key="emoji"
+    ) {{ emoji }}
+  .text-center.leading-8 Your vote ?
+  .rating.flex-row-reverse.justify-center.items-center(@change="onChange")
+    input.mask.mask-star-2.w-10.h-10(
+      v-for="item in 5"
+      type="radio"
+      name="rating"
+      :key="item"
+      :class="{active: item === myRate}"
+      :value="6 - item"
+      v-model="myRate"
+    )
 
-nuxt-link(to="/ranks") Ranks
+  .col-span-2.text-center.pt-1 Ranks
 </template>
